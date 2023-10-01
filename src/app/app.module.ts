aeebccbb3389fa596f5a90964de76f28c9f93624
cauan <cauan358@gmail.com>
2023-10-01 00:04:13 -0300

    created psn shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPriceComponent } from './components/card/card-price/card-price.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    MenuBarComponent,
+    CardLabelComponent,
+    CardPriceComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/card/card-price/card-price.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-price',
+  templateUrl: './card-price.component.html',
+  styleUrls: ['./card-price.component.css']
+})
+export class CardPriceComponent implements OnInit {
+	@Input()
+	gameType:string = "Digital PS4"
+	@Input()
+	gamePrice:string = "R$ 399,90"
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
